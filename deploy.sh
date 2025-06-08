14a0517bfd7350562cede60843ee52c034d5202b
James-M99 <Jamesmorrisonoffical@gmail.com>
2025-06-08 15:19:48 +0800

    Create deploy.sh
    Created a bash script to automatically deploy

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Exit if any command fails
+set -e
+
+# Define Variables
+REPO_URL="https://github.com/James-M99/DevOps_Portfolio.git"
+TARGET_DIR="/var/www/html"
+BACKUP_DIR="/var/www/html_backup_$(date +%Y%m%d%H%M%S)"
+
+# Clone or pull flatest from GitHub
+if [-d "/tmp/portfolio-site"]; then
+  echo "Updating existing repo..."
+  cd /tmp/portfolio-site
+  git pull
+else
+  echo "Cloning repo..."
+  git clone $REPO_URL /tmp/portfolio-site
+fi
+
+# Backup current site
+echo "Backing up existing site to $BACKUP_DIR"
+sudo cp -r $TARGET_DIR $BACKUP_DIR
+
+# Deploy new site
+echo "Deploying new site..."
+sudo cp -r /tmp/portfolio-site/* $TARGET_DIR
+
+# Set Permissions
+sudo chown -R www-data:www-data $TARGET_DIR
+sudo chown -R 755 $TARGET_DIR
+
+# Restart Apache
+echo "Restarting Apache..."
+sudo systemctl restart apache2
+echo "Deployment Complete!"
