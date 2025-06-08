4719455e131184e3ad7733b56cf929abe01ba1bf
James-M99 <Jamesmorrisonoffical@gmail.com>
2025-06-08 17:20:17 +0800

    Update backup.sh
    Added sudo to commands

--- backup.sh
+++ backup.sh
@@ -9,23 +9,23 @@ MAX_BACKUPS=3
 sudo mkdir -p "$BACKUP_DIR"
 
 # Log
-log() {
+sudo log() {
   echo "[%(date '+%d-%m-%Y %H:%M:%S')] $1" | sudo tee -a "LOGFILE" > /dev/null
 }
 
 # Create a new backup
-log "Starting backup of $SOURCE_DIR..."
+sudo log "Starting backup of $SOURCE_DIR..."
 if [ -d "$SOURCE_DIR" ]; then
   BACKUP_FILE="$BACKUP_DIR/site-backup-%(date '+%d-%m-%Y %H:%M:%S').tar.gz"
-  tar -czf "$BACKUP_FILE" "$SOURCE_DIR" 2>>"$LOGFILE"
+  sudo tar -czf "$BACKUP_FILE" "$SOURCE_DIR" 2>>"$LOGFILE"
   if [ $? -eq 0 ]; then
-    log "Backup successful!: $BACKUP_FILE"
+   sudo log "Backup successful!: $BACKUP_FILE"
     else
-    log "Backup failed during tar creation"
+    sudo log "Backup failed during tar creation"
     exit 1
   fi
 else
-  log "Source directory $SOURCE_DIR does not exist. Backup cancelled"
+ sudo log "Source directory $SOURCE_DIR does not exist. Backup cancelled"
   exit 1
 fi
 
@@ -35,8 +35,8 @@ BACKUP_COUNT=$(ls -1t "$BACKUP_DIR"/site-backup-*.tar.gz | wc -l)
 if [ "$BACKUP_COUNT" -gt "MAX_BACKUPS" ]; then
   TO_DELETE=$(ls -1t "$BACKUP_DIR"/site-backup-*.tar.gz | tall -n +$(($MAX_BACKUPS + 1)))
     for FILE in $TO_DELETE; do
-    rm -f "$FILE"
-    log "Deleted old backup: $FILE"
+    sudo rm -f "$FILE"
+    sudo log "Deleted old backup: $FILE"
     done
 fi
-log "Backup script completed"
+sudo log "Backup script completed"
