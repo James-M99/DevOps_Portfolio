40c3131a7cb1c97161e4b049c8f745701f381700
James-M99 <Jamesmorrisonoffical@gmail.com>
2025-06-08 16:12:38 +0800

    Create backup.sh
    Created a script to backup the website

---
+++ backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Declare Variables
+SOURCE_DIR="/var/www/html"
+BACKUP_DIR="/var/backups/website"
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_FILE="$BACKUP_DIR/site-backup-$TIMESTAMP.tar.gz"
+
+# Make sure backup directory exists
+mkdir -p "$BACKUP_DIR"
+
+# Create a new backup
+echo "Creating new backup of $SOURCE_DIR..."
+tar -czf "$BACKUP_FILE" "$SOURCE_DIR"
+
+# Check that backup was successful
+if [ $? -eq 0 ]; then
+  echo "Backup successful!: $BACKUP_FILE"
+else
+  echo "Backup failed"
+  exit 1
+fi
