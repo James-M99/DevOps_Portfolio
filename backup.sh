2eb692238eaa7ea9ebf1914c23e5140adae241cb
James-M99 <Jamesmorrisonoffical@gmail.com>
2025-06-08 17:34:50 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 
 # Declare Variables
+LOGFILE="/var/log/site-backup.log"
 SOURCE_DIR="/var/www/html"
 BACKUP_DIR="/var/backups/website"
-LOGFILE="/var/log/site-backup.log"
 MAX_BACKUPS=3
 # Make sure backup directory exists
-sudo mkdir -p "$BACKUP_DIR"
+mkdir -p "$BACKUP_DIR"
 
 # Log Function
 log() {
-  echo "[%(date '+%d-%m-%Y %H:%M:%S')] $1" | sudo tee -a "LOGFILE" > /dev/null
+  echo "[%(date '+%d-%m-%Y %H:%M:%S')] $1" >> "LOGFILE"
 }
 
 # Create a new backup
-sudo log "Starting backup of $SOURCE_DIR..."
+log "Starting backup of $SOURCE_DIR..."
+
 if [ -d "$SOURCE_DIR" ]; then
   BACKUP_FILE="$BACKUP_DIR/site-backup-%(date '+%d-%m-%Y %H:%M:%S').tar.gz"
-  sudo tar -czf "$BACKUP_FILE" "$SOURCE_DIR" 2>>"$LOGFILE"
+  tar -czf "$BACKUP_FILE" "$SOURCE_DIR" 2>>"$LOGFILE"
+  
   if [ $? -eq 0 ]; then
-   sudo log "Backup successful!: $BACKUP_FILE"
+   log "Backup successful!: $BACKUP_FILE"
     else
-    sudo log "Backup failed during tar creation"
+    log "Backup failed during tar creation"
     exit 1
   fi
 else
- sudo log "Source directory $SOURCE_DIR does not exist. Backup cancelled"
+ log "Source directory $SOURCE_DIR does not exist. Backup cancelled"
   exit 1
 fi
 
 # Clean up older backups
 BACKUP_COUNT=$(ls -1t "$BACKUP_DIR"/site-backup-*.tar.gz | wc -l)
 
-if [ "$BACKUP_COUNT" -gt "MAX_BACKUPS" ]; then
+if [ "$BACKUP_COUNT" -gt "$MAX_BACKUPS" ]; then
   TO_DELETE=$(ls -1t "$BACKUP_DIR"/site-backup-*.tar.gz | tall -n +$(($MAX_BACKUPS + 1)))
     for FILE in $TO_DELETE; do
-    sudo rm -f "$FILE"
-    sudo log "Deleted old backup: $FILE"
+    rm -f "$FILE"
+    log "Deleted old backup: $FILE"
     done
 fi
-sudo log "Backup script completed"
+log "Backup script completed"
