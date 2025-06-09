a6d57d14906c96c5afd7d3ada82d669a9d35199e
James-M99 <Jamesmorrisonoffical@gmail.com>
2025-06-09 12:10:52 +0800

    Update tag-test.sh
    Added comments for clarity

--- scripts/tag-test.sh
+++ scripts/tag-test.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-
+# Define Variables
 REPO_DIR="/home/ubuntu/DevOps_Portfolio"
 VERSION="v$(date '+%d%m%Y-%H%M%S')"
 
+# Change directory
 echo "Changing to $REPO_DIR"
 cd "$REPO_DIR" || { echo "Failed to cd into $REPO_DIR"; exit 1; }
 
+# Get Git status
 echo "Current Git status:"
 git status
 
+# Create a tag
 echo "Creating tag: $VERSION"
 git tag "$VERSION"
 
+# Show existing tags
 echo "Existing tags:"
 git tag
